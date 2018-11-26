32976a33de99cfaab2e8dab33e119498376be7f8
Evan Bassler <evan@cuberis.com>
2018-11-26 09:51:06 -0500

    remove unnecessary js and images

--- wp-content/themes/classics/assets/scripts/routes/home.js
+++ wp-content/themes/classics/assets/scripts/routes/home.js
@@ -1,16 +1,8 @@
 // JavaScript to be fired on the home page
 
-import smoothScrollLinks from "../util/smoothScrollLinks";
-import carouselNormalization from "../util/carouselNormalization";
-
 export default {
   init() {
-    // Initialize smooth scroll "jump links"
-    smoothScrollLinks();
-    $(".carousel").carousel({
-      interval: 10000
-    });
-    carouselNormalization();
+
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
